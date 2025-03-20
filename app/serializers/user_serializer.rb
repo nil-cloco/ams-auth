08c56c511e7e9a9c0deae2dd6b0874acd4014d7b
nil_gaudel <gaudel.nil@cloco.net>
2025-03-20 10:41:25 +0545

    add role in user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,7 @@
 class UserSerializer
   include JSONAPI::Serializer
-  attributes :id, :first_name, :last_name, :email, :gender, :phone, :dob, :address, :created_at
+  attributes :id, :first_name, :last_name, :email, :gender, :phone, :dob, :address, :created_at, :role
   attribute :created_date do |user|
-    user.created_at && user.created_at.strftime('%m/%d/%Y')
+    user.created_at && user.created_at.strftime("%m/%d/%Y")
   end
 end
